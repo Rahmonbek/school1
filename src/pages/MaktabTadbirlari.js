8b178ddb8da742c5970f7bd615b7ce905e9ef750
Rahmonbek <83601261+Rahmonbek@users.noreply.github.com>
2021-10-04 16:33:51 +0500

    mkmklm

--- src/pages/MaktabTadbirlari.js
+++ src/pages/MaktabTadbirlari.js
@@ -291,7 +291,7 @@ export default class MaktabTadbirlari extends Component {
                     height="400px"
                     // style={{marginLeft:"10%"}}
                     state={{
-                      center: [39.651698, 66.97187],
+                      center: [41.552486, 60.620890],
                       zoom: 13,
                     }}
                   >
@@ -302,7 +302,7 @@ export default class MaktabTadbirlari extends Component {
                     >
                       <Placemark
                         key={-1}
-                        geometry={[39.651698, 66.97187]}
+                        geometry={[41.552486, 60.620890]}
                         options={{
                           iconLayout: "default#image",
                         }}
