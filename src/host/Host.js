9fcf5dc330d552af69779b67f045881df271d2f9
jamshid-zayniyev <jamshidzayniev10@gmail.com>
2022-06-18 23:54:45 +0500

    dadsda
    sdfsd

--- src/host/Host.js
+++ src/host/Host.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const url = "http://62.209.143.146:8003";
+export const url = "https://api.maktab.abrorjonaxmadov.uz";
 export const idMaktab = "7";
 export const user = "44";
 export const httpRequest = (config) => {
